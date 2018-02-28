fb3144c790467d0f5195c86bb8ca69306dd7d334
CxyBlue <1940579695@qq.com>
2018-02-28 13:46:09 +0800

    添加图片控件

--- GitDemo/ViewController.m
+++ GitDemo/ViewController.m
@@ -9,7 +9,7 @@
 #import "ViewController.h"
 
 @interface ViewController ()
-
+@property (nonatomic,strong)UIImageView * headImgView;
 @end
 
 @implementation ViewController
@@ -17,11 +17,25 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
-    self.view.backgroundColor = [UIColor redColor];
+    self.view.backgroundColor = [UIColor yellowColor];
     
     self.view.frame = CGRectMake(0, 0, 50, 40);
+    
+    [self createUI];
+    
 }
 
+-(void)createUI{
+    
+    _headImgView = [[UIImageView alloc]init];
+    _headImgView.frame = CGRectMake(20, 60, 100, 100);
+    _headImgView.layer.cornerRadius = 100/2;
+    _headImgView.layer.masksToBounds = YES;
+    _headImgView.layer.borderColor = [UIColor blueColor].CGColor;
+    _headImgView.layer.borderWidth = 1;
+    
+    [self.view addSubview:_headImgView];
+}
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
